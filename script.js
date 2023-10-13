fc5564a5a16b2319d5bc3a3275b3e7690205f8ff
robynae <92494880+robynae@users.noreply.github.com>
2023-10-12 19:00:47 -0500

    Create changeColor function

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 const container = document.querySelector('.container');
+
 function createGrid(num) {
     for (let i = 0; i < num; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
         container.appendChild(row);
-        for(let j = 0; j < num; j++) {
+        for (let j = 0; j < num; j++) {
             const cells = document.createElement('div');
             cells.classList.add('cells');
             row.appendChild(cells);
         }
     }
-};
+}
+
+function changeColor(cell) {
+
+            cell.style.backgroundColor = 'black';
+    };
 
 createGrid(16);
+
+const cells = document.querySelectorAll('.cells');
+
+cells.forEach(cell => {
+    cell.addEventListener('mouseover', () => {
+        changeColor(cell);
+    });
+});
+
+
+
+
+
+
+
+
+
 
