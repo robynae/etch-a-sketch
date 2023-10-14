e930945daf1c8d5ab5fd584fd32475b189e6c3dd
robynae <92494880+robynae@users.noreply.github.com>
2023-10-14 16:49:54 -0500

    Create grid buttons, color buttons, and reset button

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const container = document.querySelector('.container');
+let currentGridSize = 16;
+let currentColor = 'black';
 
-function createGrid(num) {
+function createGrid(num = 16) {
+    num = currentGridSize;
     for (let i = 0; i < num; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
@@ -13,27 +16,85 @@ function createGrid(num) {
     }
 }
 
-function changeColor(cell) {
+function changeColor(cell, color) {
+    color = currentColor;
+    cell.style.backgroundColor = currentColor;
+}
 
-            cell.style.backgroundColor = 'black';
-    };
+createGrid();
 
-createGrid(16);
-
-const cells = document.querySelectorAll('.cells');
-
-cells.forEach(cell => {
-    cell.addEventListener('mouseover', () => {
-        changeColor(cell);
+function pleaseWork() {
+    container.addEventListener('mouseover', (event) => {
+        if (event.target.classList.contains('cells')) {
+            changeColor(event.target);
+        }
     });
+}
+
+pleaseWork();
+
+function newGrid() {
+    container.innerHTML = '';
+}
+
+const thirtyTwo = document.querySelector('.thirty-two');
+const sixteen = document.querySelector('.sixteen');
+const sixtyFour = document.querySelector('.sixty-four');
+const buttons = document.querySelectorAll('.button')
+
+function newSize(event) {
+  if(event.target.classList.contains('sixteen')) {
+    currentGridSize = 16;
+    newGrid();
+    createGrid(currentGridSize); 
+    pleaseWork();
+  } else if(event.target.classList.contains('thirty-two')) {
+    currentGridSize = 32;
+    newGrid();
+    createGrid(currentGridSize);
+    pleaseWork();
+  } else if(event.target.classList.contains('sixty-four')) {
+    currentGridSize = 64;
+    newGrid();
+    createGrid(currentGridSize);
+    pleaseWork();
+  }
+
+}
+
+buttons.forEach(function(button) {
+    button.addEventListener('click', newSize);
 });
 
+const reset = document.querySelector('.reset');
 
+function resetGrid() {
+  newGrid();
+  createGrid(currentGridSize);
+  pleaseWork();
+};
 
+reset.addEventListener('click', resetGrid);
 
+const red = document.querySelector('.red');
+const blue = document.querySelector('.blue');
+const orange = document.querySelector('.orange');
+const colors =document.querySelectorAll('.color')
 
+function colorMode(event) {
+  switch(event.target.classList[0]) {
+    case 'red':
+        currentColor = '#ca3c4a';
+        break;
+    case 'orange':
+        currentColor = '#ef7546';
+        break;
+    case 'blue':
+        currentColor = '#99beb7';
+        break;
+  }
+}
 
-
-
-
-
+colors.forEach(function(color) {
+    color.addEventListener('click', colorMode);
+});
